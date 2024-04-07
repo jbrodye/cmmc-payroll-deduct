22a64658dbdcb0a4975b7f4aa2295b2a76ab2159
jbrodye <jbrodye@icloud.com>
2024-04-06 19:17:57 -0500

    Can delete transactions

--- src/routes/api/transaction/+server.ts
+++ src/routes/api/transaction/+server.ts
@@ -7,39 +7,39 @@ export const GET: RequestHandler = async function ({ platform }) {
 }
 
 export const POST: RequestHandler = async function ({ request, platform }) {
-  const { employee_id, dollars, cents, tyear, tmonth, tday, thour, tminute } = await request.json();
+  const { op } = await request.json();
 
-  try {
-    await platform?.env.DB.prepare('INSERT INTO transactions (id, employee_id, dollars, cents, tyear, tmonth, tday, thour, tminute) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?)')
-                            .bind(employee_id, dollars, cents, tyear, tmonth, tday, thour, tminute)
-                            .run();
-    
+  if(op.action == "insert") {
+    try {
+      await platform?.env.DB.prepare('INSERT INTO transactions (id, employee_id, dollars, cents, tyear, tmonth, tday, thour, tminute) VALUES (NULL, ?, ?, ?, ?, ?, ?, ?, ?)')
+                              .bind(op.data.employee_id, op.data.dollars, op.data.cents, op.data.tyear, op.data.tmonth, op.data.tday, op.data.thour, op.data.tminute)
+                              .run();
+      
       return json({
         success: true
       });
 
-  } catch (err) {
-    return new Response(err, {
-      status: 500
-    });
+    } catch (err) {
+      return new Response(err, {
+        status: 500
+      });
+    }
+  }
+
+  if(op.action == "delete") {
+    try {
+      await platform?.env.DB.prepare('DELETE FROM transactions WHERE id = ?')
+                              .bind(op.data.transaction_id)
+                              .run();
+  
+      return json({
+        success: true
+      });
+  
+    } catch (err) {
+      return new Response(err, {
+        status: 500
+      });
+    }
   }
 };
-
-export const DELETE: RequestHandler = async function ({ request, platform }) {
-  const { transaction_id } = await request.json();
-
-  try {
-    await platform?.env.DB.prepare('DELETE FROM transactions WHERE id = ?')
-                            .bind(transaction_id)
-                            .run();
-
-      return json({
-        success: true
-      });
-
-  } catch (err) {
-    return new Response(err, {
-      status: 500
-    });
-  }
-}
--- src/routes/manage/+page.svelte
+++ src/routes/manage/+page.svelte
@@ -1,14 +1,69 @@
 <script lang="ts">
   import { page } from '$app/stores';
+  import { Table, TableBody, TableBodyCell, TableBodyRow, TableHead, TableHeadCell } from 'flowbite-svelte';
+
+  async function deleteTransaction(id) {
+    const response = await fetch("/api/transaction", {
+      method: "POST",
+      headers: {
+        "Content-Type": "application/json",
+      },
+
+      body: JSON.stringify({
+        "op": {
+          "action": "delete",
+          "data": {
+            "transaction_id": id
+        }
+    }
+})
+    });
+
+    const data = await response.json();
+    
+    if(data && data.success == true) {
+        location.reload();
+    }
+  }
 </script>
 
 <section>
-  <h1>Transactions</h1>
-  {#each $page.data.transactions.results as { id, employee_id, dollars, cents, time }}
-    Transaction: ID {id}, Employee: {employee_id}, Amount: ${dollars}.{cents}, Time: {time}<br>
-  {/each}
+  <center><b>TRANSACTION HISTORY</b></center>
+  <br />
+  <div class="transactionTable">
+    <Table shadow striped={true} hoverable={true}>
+      <TableHead>
+        <TableHeadCell>Transaction ID</TableHeadCell>
+        <TableHeadCell>Employee ID</TableHeadCell>
+        <TableHeadCell>Amount</TableHeadCell>
+        <TableHeadCell>Time</TableHeadCell>
+        <TableHeadCell>Actions</TableHeadCell>
+      </TableHead>
+      <TableBody class="divide-y">
+      {#each $page.data.transactions.results as { id, employee_id, dollars, cents, tyear, tmonth, tday, thour, tminute }}
+        <TableBodyRow>
+          <TableBodyCell>{id}</TableBodyCell>
+          <TableBodyCell>{employee_id}</TableBodyCell>
+          <TableBodyCell>${dollars}.{cents.toString().padStart(2, "0")}</TableBodyCell>
+          <TableBodyCell>{tday.toString().padStart(2, "0")}/{tmonth.toString().padStart(2, "0")}/{tyear} {thour.toString().padStart(2, "0")}:{tminute.toString().padStart(2, "0")}</TableBodyCell>
+          <TableBodyCell>
+            <button on:click={deleteTransaction(id)} data-sveltekit-reload>Delete Transaction</button>
+          </TableBodyCell>
+        </TableBodyRow>
+      {/each}
+      </TableBody>
+    </Table>
+  </div>
 </section>
 
 <style>
-  /* Place styles of your view here */
+  :global(body) {
+    margin-top: 16px;
+  }
+
+  div.transactionTable {
+    width: 100%;
+    padding-left: 16px;
+    padding-right: 16px;
+  }
 </style>
